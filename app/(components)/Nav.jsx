1616c16a00e2c39a10e439ddd12c509a7bd15993
Priyansh <PriyanshYadav1012@gmail.com>
2024-01-01 09:41:50 +0530

    Adding Styles

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,8 +1,24 @@
+import { faHome, faTicket, faTractor } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <div>
+        <Link href="/">
+            <FontAwesomeIcon icon={faHome} className='icon' />
+        </Link>
+        <Link href="/TicketPage/new">
+            <FontAwesomeIcon icon={faTicket} className='icon' />
+        </Link>
+      </div>
+      <div>
+        <p className=' text-default-text'>Priyansh1012@gmail.com</p>
+      </div>
+    </nav>
+    
   )
 }
 
